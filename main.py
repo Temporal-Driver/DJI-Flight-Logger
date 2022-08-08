5015cc150863fa8db950ad5e3df538077af02124
Eric <temporaldriver@outlook.com>
2022-08-07 20:31:53 -0400

    +cleaned up code a bit +added directory validation +added config (settings.ini) still technically prototyping, and currently prints a list of sorted classes containing footage data

--- main.py
+++ main.py
@@ -1,47 +1,56 @@
-# DJI-Flight-Logger
-# github.com/temporal-driver/dji-flight-logger
 import os
 import configparser
+import sys
 import time
 import data
 from pathlib import Path
 
 config = configparser.ConfigParser()
 config.read('settings.ini')
-working_dir = config['PATHS']['footage_dir']
-files = []
+footage_dir = config['PATHS']['footage_directory']
 
 
-def epoch_convert(sec):  # converts epoch seconds to YYYY/MM/DD
-    filedate = time.strftime('%Y/%m/%d', time.gmtime(sec))
-    return filedate
+def main():
+    check_dir()
+    files = read_files()
+    for f in files:
+        print(f.fileName)
+
+
+def check_files():
+    logfile = open(os.getcwd() + "\\filelog.txt")
 
 
 def read_files():
-    for rf in os.listdir(working_dir):
+    presorted_files = []
+    for rf in os.listdir(footage_dir):
         if rf.endswith('.MP4' or '.JPG'):
             f_name = os.path.basename(rf)
-            f_path = os.path.join(working_dir, f_name)
+            f_path = os.path.join(footage_dir, f_name)
             f_size = os.path.getsize(f_path)
             f_time = os.path.getmtime(f_path)
-            f_date = epoch_convert(f_time)
+            f_date = time.strftime('%Y/%m/%d', time.gmtime(f_time))
             c_name = Path(f_path).stem
             locals()[c_name] = data.Footage(f_name, f_size,
                                             f_time, f_date, f_path)
-            files.append(locals()[c_name])
+            presorted_files.append(locals()[c_name])
+    files = sorted(presorted_files, key=lambda f: f.fileTime)
+    return files
 
 
-def sort_by_time():
-    sorted_files = sorted(files, key=lambda f: f.fileTime)
-    return sorted_files
+def check_dir():  # Checks if the directory in settings.ini exists, then checks if there's footage.
+    if os.path.isdir(footage_dir):
+        footage = 0
+        for f in os.listdir(footage_dir):
+            if f.endswith('.MP4' or '.JPG'):
+                footage += 1
+        if footage > 0:
+            return
+    else:
+        print("Directory path invalid or no footage was found. Please check 'settings.ini' and try again.")
+        input("Press enter to exit;")
+        sys.exit()
 
 
 if __name__ == '__main__':
-    read_files()
-    for fd in files:
-        print(fd.fileDate)
-
-def count_dates():
-    dates = []
-    for d in files:
-        dates.append(d.flightTime)
+    main()
